c8ac9b778671ed003f89a712e58cc7a8acbdc936
wyatt987 <526wbedor@frhsd.com>
2025-01-08 11:56:35 -0500

    Starting onclick java

---
+++ script.js
@@ -0,0 +1,20 @@
+/* First button*/
+const showWebsite = document.getElementById('yes');
+const RPS = document.getElementById('RPS');
+const Choice = document.getElementById('Choice');
+
+
+/*Reveal website*/
+showWebsite.addEventListener('click', () => {
+    RPS.classList.remove('d-none');
+    Choice.classList.remove('d-none');
+    storySection.scrollIntoView({ behavior: 'smooth' });
+});
+function userChoice() {
+    User === "Rock"
+}
+
+
+
+document.getElementById('User').innerHTML = User
+
