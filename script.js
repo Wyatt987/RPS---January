36be3502f8adcc44b34d518f77a993a9767652e3
wyatt987 <526wbedor@frhsd.com>
2025-01-10 09:31:42 -0500

    fix computer

--- script.js
+++ script.js
@@ -10,13 +10,15 @@ showWebsite.addEventListener('click', () => {
     Choice.classList.remove('d-none');
     RPS.scrollIntoView({ behavior: 'smooth' });
 });
+
+//User Choice//
 let user = document.getElementById('user');
 document.getElementById('rocky').addEventListener('click', userChoice);
 document.getElementById('Papery').addEventListener('click', userChoice);
 document.getElementById('Scissory').addEventListener('click', userChoice);
 
 function userChoice(event) {
-    let clickedButtonId = event.target.parentElement.id; // Get the ID of the clicked button
+    let clickedButtonId = event.target.parentElement.id; 
 
     if (clickedButtonId === 'rocky') {
         user.textContent = "Rock";
@@ -31,8 +33,20 @@ function userChoice(event) {
         console.log("Invalid choice");
     }
 }
-
-
+//ComputerChoice//
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+    const computerChoice = getComputerChoice();
+    document.getElementById('CC').innerHTML = computerChoice;
+}
 
 
 
