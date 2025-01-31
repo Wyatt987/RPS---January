15d8f23c20cfc517c0bad2d1cfdd096532fd31df
wyatt987 <526wbedor@frhsd.com>
2025-01-31 09:16:33 -0500

    Responsive

--- script.js
+++ script.js
@@ -101,3 +101,6 @@ document.getElementById('Rocky').addEventListener('click', () => playGame('Rocky
 document.getElementById('Papery').addEventListener('click', () => playGame('Papery'));
 document.getElementById('Scissory').addEventListener('click', () => playGame('Scissory'));
 
+function userName() {
+    headerr.classList.add('d-none');
+}
